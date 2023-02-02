f888ec84f23f91c18c64679d575234e53c3e8171
krmmzs <belliyan250@gmail.com>
2023-02-02 23:55:10 +0800

    done with lock: Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -12,31 +12,33 @@
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+// defined by kernel.ld.
 
 struct run {
-  struct run *next;
+    struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+    struct spinlock lock;
+    struct run *freelist;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    int i;
+    for (i = 0; i < NCPU; i++)
+        initlock(&kmem[i].lock, "kmem");
+    freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+        kfree(p);
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,20 +48,25 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    push_off();
+    int id = cpuid();
+
+    acquire(&kmem[id].lock);
+    r->next = kmem[id].freelist;
+    kmem[id].freelist = r;
+    release(&kmem[id].lock);
+
+    pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +75,37 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    push_off();
+    int id = cpuid();
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    acquire(&kmem[id].lock);
+    r = kmem[id].freelist;
+    if(r)
+        kmem[id].freelist = r->next;
+    release(&kmem[id].lock);
+    if (!r) {
+        // Try to steal from other cpus's freelist.
+        int i;
+        for (i = 0; i < NCPU; i ++) {
+            if (i == id) {
+                continue;
+            }
+            acquire(&kmem[i].lock);
+            r = kmem[i].freelist;
+            if (r) {
+                kmem[i].freelist = r->next;
+                release(&kmem[i].lock);
+                break;
+            }
+            release(&kmem[i].lock);
+        }
+    }
+
+    pop_off();
+
+    if(r)
+        memset((char*)r, 5, PGSIZE); // fill with junk
+    return (void*)r;
 }
